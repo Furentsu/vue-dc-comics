f2445c5d851f24920c131c73b3bb9365fb8fe5e4
Furentsu <eugeniofranzoso@gmail.com>
2021-09-24 16:41:32 +0200

    True initialization

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div id="app">
-    
+    <Header />
+    <Main />
+    <FooterTop />
+    <FooterBottom />
   </div>
 </template>
 
 <script>
+import Header from './components/header.vue'
+import Main from './components/main.vue'
+import FooterTop from './components/footer-top.vue'
+import FooterBottom from './components/footer-bottom.vue'
 
 export default {
   name: 'App',
-  
+  components: {
+    Header,
+    Main,
+    FooterTop,
+    FooterBottom
+  }
 }
-</script>
+</script> 
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+@import "./style/variables.scss";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
 #app {
-  font-family: 'Open Sans', sans-serif;
+  font-family: '$primaryFont', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
